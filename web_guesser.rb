da43c227880c5175feb451b497fa9f0af27c71ca
Ali Schlereth <aschlere@sbcglobal.net>
2016-09-26 21:41:42 -0600

    Extension Colorizing the Output

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 # set :num, rand(101)
 SECRET_NUMBER = rand(100)
@@ -10,14 +11,25 @@ def check_guess(guess)
   when (params['guess'].to_i + 5) < SECRET_NUMBER then "Way too low!"
   when params['guess'].to_i > SECRET_NUMBER       then "Too high!"
   when params['guess'].to_i < SECRET_NUMBER       then "Too low!"
-  when params['guess'].to_i == SECRET_NUMBER      then "You go it right!"
+  when params['guess'].to_i == SECRET_NUMBER      then "You got it right!"
+  end
+end
+
+def check_for_color(message)
+  if message == "Way too high!" || message == "Way too low!"
+    "red"
+  elsif message == "Too high!"  || message==  "Too low!"
+    "pink"
+  else
+    "green"
   end
 end
 
 get '/' do
   guess = params['guess']
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  color = check_for_color(message)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
 end
 
 # def assign_number
